e6b03e4674894e6885e8be3e723a33fb9da88e8f
João Victor <54009514+T4SUYA@users.noreply.github.com>
2019-12-29 01:37:57 -0300

    backend authentication done

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
-const moongose = require('mongoose');
+const moongose = require('mongoose')
 
-const authRoute = require('./routes/auth');
+const authRoute = require('./routes/auth')
+const postsRoute = require("./routes/posts")
 
-dotenv.config();
+dotenv.config()
 
-app.use(express.json());
+app.use(express.json())
 
 moongose.connect(process.env.DB_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, 
-() => console.log('conected to db!'));
+() => console.log('conected to db!'))
 
 
-app.use('/api/user', authRoute);
+app.use('/api/user', authRoute)
+app.use('/api/posts', postsRoute)
 
-app.listen(3000, () => console.log("Up and running"));
+app.listen(3000, () => console.log("Up and running"))
